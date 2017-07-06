d1fd8a59048a1ae26c1460752e9d8915379e3248
Claire Chen <cokeity@fb.com>
2017-07-06 11:34:56 -0700

    implemented user persistence

--- twitter_alamofire_demo/AppDelegate.swift
+++ twitter_alamofire_demo/AppDelegate.swift
@@ -17,6 +17,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         // MARK: TODO: Check for logged in user
+        if User.current != nil {
+            //user exists
+            // Load and show the login view controller
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let homeTimelineViewController = storyboard.instantiateViewController(withIdentifier: "TweetsNavigationController")
+            window?.rootViewController = homeTimelineViewController
+        } else {
+            //take to login
+        }
         
         NotificationCenter.default.addObserver(forName: Notification.Name("didLogout"), object: nil, queue: OperationQueue.main) { (Notification) in
             print("Logout notification received")
--- twitter_alamofire_demo/User.swift
+++ twitter_alamofire_demo/User.swift
@@ -16,14 +16,43 @@ class User {
     //initialize extra properties
     
     //static property
-    static var current: User?
+    //static var current: User?
+    
+    var dictionary: [String: Any]?
+    private static var _current: User?
     
     
     init(dictionary: [String: Any]) {
+        self.dictionary = dictionary
         name = dictionary["name"] as? String
         screenName = dictionary["screen_name"] as? String
         profilePicString = dictionary["profile_image_url_https"] as? String
         //initialize extra properties
 
     }
+    
+    static var current: User? {
+        get {
+            if _current == nil {
+                let defaults = UserDefaults.standard
+                if let userData = defaults.data(forKey: "currentUserData") {
+                    let dictionary = try! JSONSerialization.jsonObject(with: userData, options: []) as! [String: Any]
+                    _current = User(dictionary: dictionary)
+                }
+            }
+            return _current
+        }
+        set (user) {
+            _current = user
+            let defaults = UserDefaults.standard
+            if let user = user {
+                let data = try! JSONSerialization.data(withJSONObject: user.dictionary!, options: [])
+                defaults.set(data, forKey: "currentUserData")
+            } else {
+                defaults.removeObject(forKey: "currentUserData")
+            }
+        }
+    }
+    
+    
 }
